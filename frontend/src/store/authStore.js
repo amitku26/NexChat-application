ac28797adf0fbcf1381b81c0c6cd5c44d410aebe
amitkumar2026 <amitkumar829448@gmail.com>
2026-01-03 10:18:32 +0530

    url added

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -4,7 +4,9 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+  import.meta.env.MODE === "development"
+    ? "https://nexchat-application.onrender.com/"
+    : "/";
 
 export const authStore = create((set, get) => ({
   loggedUser: null,
