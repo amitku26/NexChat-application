a43a1fb97de28600aba18978db3fcebf4cec512a
amitkumar2026 <amitkumar829448@gmail.com>
2026-01-03 13:40:19 +0530

    url fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,8 +5,6 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
-//import path from "path";
-
 dotenv.config();
 
 app.use(express.json({ limit: "10mb" }));
@@ -16,7 +14,9 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:[ "http://localhost:5173",
+      "https://nexchat-application-1.onrender.com"
+    ],
     credentials: true,
   })
 );
@@ -31,17 +31,8 @@ import messageRoute from "./routes/messageRoute.js";
 app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-// })
-// }
 
 const port = process.env.PORT || 5000;
-//const __dirname = path.resolve();
 
 mongoose
   .connect(process.env.MONGODB_URI)
