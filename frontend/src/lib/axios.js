e0dde564a175f7db8b6aba171b5ba4e2fc1a545e
amitkumar2026 <amitkumar829448@gmail.com>
2026-01-03 13:15:10 +0530

    fixed error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,16 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL:
+//     import.meta.env.MODE === "development"
+//       ? "https://nexchat-application.onrender.com/api"
+//       : "/api",
+//   withCredentials: true,
+// });
+
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "https://nexchat-application.onrender.com/api"
-      : "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
