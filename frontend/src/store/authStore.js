47c416101b0247ad8d6301d33e3a6c57f409feca
amitkumar2026 <amitkumar829448@gmail.com>
2026-01-03 21:34:34 +0530

    url error fix

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -92,7 +92,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_API_URL.replace("/api", "");
+const SOCKET_URL = import.meta.env.VITE_API_URL;
 
 export const authStore = create((set, get) => ({
   loggedUser: null,
